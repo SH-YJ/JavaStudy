fc3095f88fd31119ae1f5204387bf267adda6597
SH-YJ <2973211152@qq.com>
2020-11-07 15:30:34 +0800

    update

--- src/com/Study/Data_Structure/T_BitSet.java
+++ src/com/Study/Data_Structure/T_BitSet.java
@@ -1,4 +1,35 @@
 package com.Study.Data_Structure;
 
+import java.util.BitSet;
+
 public class T_BitSet {
+    public static void main(String[] args) {
+        BitSet bits1 = new BitSet(16);  // 设置初始大为16
+        BitSet bits2 = new BitSet(16);
+
+        // 给一些位赋值
+        for (int i = 0; i < 16; i++) {
+            if ((i % 2) == 0) bits1.set(i);
+            if ((i % 5) != 0) bits2.set(i);
+        }
+        System.out.println("Initial pattern in bits1:");
+        System.out.println(bits1);
+        System.out.println("\nInitial pattern in bits2:");
+        System.out.println(bits2);
+
+        // AND bits 逻辑与
+        bits2.and(bits1);
+        System.out.println("\nbits2 AND bits1:");
+        System.out.println(bits2);
+
+        // OR bits 逻辑或
+        bits2.or(bits1);
+        System.out.println("\nbits2 OR bits1:");
+        System.out.println(bits2);
+
+        // XOR bits 逻辑异或
+        bits2.xor(bits1);
+        System.out.println("\nbits2 XOR bits1:");
+        System.out.println(bits2);
+    }
 }
--- src/com/Study/Data_Structure/T_Vector.java
+++ src/com/Study/Data_Structure/T_Vector.java
@@ -1,4 +1,40 @@
 package com.Study.Data_Structure;
 
+import java.util.Vector;
+import java.util.Enumeration;
+
 public class T_Vector {
+    public static void main(String[] args) {
+        // 初始容量 3,增量 2
+        Vector<Object> v = new Vector<>(3,2);
+        //System.out.println("初始大小：" + v.size());组件的数量初始为0，该程序最后为11
+        System.out.println("初始容量：" + v.capacity());
+        v.addElement(1);//添加组件
+        v.addElement(2);
+        v.addElement(3);
+        v.addElement(4);
+        System.out.println("增加4个数后的容量：" + v.capacity());
+
+        v.addElement(5.45);
+        System.out.println("当前容量：" + v.capacity());
+        v.addElement(6.08);
+        v.addElement(7);
+        System.out.println("当前容量" + v.capacity());
+        v.addElement(9.4);
+        v.addElement(10);
+        System.out.println("当前容量：" + v.capacity());
+        v.addElement(11);
+        v.addElement(12);
+        System.out.println("首元素：" + v.firstElement());
+        System.out.println("末尾元素：" + v.lastElement());
+        if (v.contains(3)){
+            System.out.println("Vector 包含 3.");
+        }
+
+        Enumeration<Object> vEnum = v.elements();
+        System.out.println("\nVector 中的元素：");
+        while (vEnum.hasMoreElements()){
+            System.out.print(vEnum.nextElement() + " ");
+        }
+    }
 }
