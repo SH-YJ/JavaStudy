b1c51a1e8a007179ceec0056fc26cfd2012c2163
SH-YJ <2973211152@qq.com>
2020-12-10 17:35:55 +0800

    update

---
+++ src/com/Study/HashSet/Test1.java
@@ -0,0 +1,21 @@
+package com.Study.HashSet;
+
+import java.util.HashSet;
+
+/**
+ * @author SH_YJ
+ * @date 2020/12/10 17:07
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        HashSet<String> set = new HashSet<>();
+        set.add("a");
+        set.add("b");
+        set.add("c");
+        System.out.println(set.contains("a"));
+        for (String str :
+                set) {
+            System.out.println(str);
+        }
+    }
+}
---
+++ src/com/Study/LinkedList/Test1.java
@@ -0,0 +1,23 @@
+package com.Study.LinkedList;
+
+import java.util.LinkedList;
+
+/**
+ * @author SH_YJ
+ * @date 2020/12/10 16:22
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+
+//        list.addFirst("First");
+//        list.addLast("Last");
+//        list.removeLast();
+//        list.removeFirst();
+        System.out.println(list);
+
+    }
+}
